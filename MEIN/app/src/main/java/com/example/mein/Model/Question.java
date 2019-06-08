5b906a203e59784cd3db646424a651f6503fa09e
Thomas <thomas.bonin@th-bingen.de>
2019-06-08 15:24:42 +0200

    Frageklasse

---
+++ MEIN/app/src/main/java/com/example/mein/Model/Question.java
@@ -0,0 +1,20 @@
+
+
+public class Question{
+    public String Question;
+    public String choice1;
+    public String choice2;
+    public String choice3;
+    public String choice4;
+
+    public int[] correctAnswers;
+
+    public Question(String question, String c1, String c2, String c3, String c4, int[] correctAnswers){
+        Question=question;
+        choice1=c1;
+        choice2=c2;
+        choice3=c3;
+        choice4=c4;
+        this.correctAnswers=correctAnswers;
+    }
+}
