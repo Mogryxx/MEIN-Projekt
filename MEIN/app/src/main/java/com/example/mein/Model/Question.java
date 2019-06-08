e4243361f5d4b2245b682f8eb60fd25291c9d51a
Thomas <thomas.bonin@th-bingen.de>
2019-06-08 15:58:32 +0200

    Frageklasse mit Fehlerbehebung

--- MEIN/app/src/main/java/com/example/mein/Model/Question.java
+++ MEIN/app/src/main/java/com/example/mein/Model/Question.java
@@ -1,20 +1,32 @@
 package com.example.mein.Model;
 
-public class Question{
+import java.util.LinkedList;
+
+public class Question {
     public String Question;
     public String choice1;
     public String choice2;
     public String choice3;
     public String choice4;
 
-    public int[] correctAnswers;
+    public LinkedList<Integer> correctAnswers;
 
-    public Question(String question, String c1, String c2, String c3, String c4, int[] correctAnswers){
-        Question=question;
-        choice1=c1;
-        choice2=c2;
-        choice3=c3;
-        choice4=c4;
-        this.correctAnswers=correctAnswers;
+    public Question(String question, String c1, String c2, String c3, String c4, LinkedList<Integer> correctAnswers) {
+        Question = question;
+        choice1 = c1;
+        choice2 = c2;
+        choice3 = c3;
+        choice4 = c4;
+        this.correctAnswers = correctAnswers;
+    }
+
+    public boolean isCorrectAnswer(int[] ans) {
+        boolean correct = true;
+        for (int i : ans) {
+            if (!correctAnswers.contains(i))
+                return false;
+        }
+
+        return correct;
     }
 }
